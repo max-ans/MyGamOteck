9b202e50c3ad1f392f744c4f6b360a1f7800baf7
Max-ans <maxenceroyer@hotmail.com>
2020-06-21 18:06:37 +0200

    Création d'un formulaire sans le traiter

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Game;
+use App\Form\GameType;
 use App\Repository\GameRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GameController extends AbstractController
@@ -29,6 +31,27 @@ class GameController extends AbstractController
         $game = $this->gameRepository->findOneWithData($id);
         return $this->render('game/show.html.twig', [
             'game' => $game
+        ]);
+    }
+
+
+    /**
+     * @Route("/add" , name="game_add")
+     */
+    public function add (Request $request) {
+
+        $newGame = new Game();
+
+        $form = $this->createForm(GameType::class, $newGame);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            dump($newGame);
+        }
+
+        return $this->render('game/add.html.twig', [
+            'formView' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/GameType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Game;
+use App\Entity\Support;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title',null ,
+            [
+                'label' => 'Nom du Jeu'
+            ])
+            ->add('description', TextareaType::class, 
+            [
+                'label' => 'Description du jeux'
+            ])
+            ->add('image', FileType::class,
+            [ 
+                'label' => 'Uploader une image du jeu'
+            ] )
+            ->add('note', IntegerType::class , 
+            [
+                'label' => 'Note sur dix'
+            ])
+            ->add('insertedAt', DateType::class, 
+            [
+                'label' => 'Depuis quand avez vous le jeu',
+                'widget' => 'single_text'
+            ] )
+            ->add('supports', EntityType::class, 
+            [
+                'class' => Support::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Game::class,
+        ]);
+    }
+}
