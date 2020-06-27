705ec1fd95ea65ba5621f6cbbf2c767c9b940ce8
max-ans <maxenceroyer@hotmail.com>
2020-06-27 15:14:01 +0200

    Crud complet sur Category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use App\Service\Slugger;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
@@ -13,12 +16,14 @@ class CategoryController extends AbstractController
 
     private $categoryRepository;
     private $manager;
+    private $slugger;
     
 
-    public function __construct(CategoryRepository $categoryRepository, EntityManagerInterface $manager)
+    public function __construct(CategoryRepository $categoryRepository, EntityManagerInterface $manager, Slugger $slugger)
     {   
         $this->categoryRepository = $categoryRepository;
         $this->manager = $manager;
+        $this->slugger = $slugger;
                 
     }
 
@@ -35,6 +40,34 @@ class CategoryController extends AbstractController
             'allCategories' => $allCategories ,
         ]);
     }
+    /**
+     * @Route("/category/add" , name="category_add")
+     */
+    public function add (Request $request)
+    {
+        $newCategory = new Category();
+
+        $form = $this->createForm( CategoryType::class ,$newCategory);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+           
+            $newCategory->setSlug($this->slugger->slugify($newCategory->getName()));
+
+            $this->manager->persist($newCategory);
+
+            $this->manager->flush();
+
+            $this->addFlash('success', 'La catégorie a bien été enregistée');
+
+            return $this->redirectToRoute('category');
+
+        }
+
+        return $this->render('category/form.html.twig', [
+            'form' => $form->createView(),
+        ] );
+    }
 
     /**
      * @Route("/category/{slug}" , name="category_show")
@@ -44,5 +77,49 @@ class CategoryController extends AbstractController
         return $this->render('category/show.html.twig',[
             'category' => $category
         ]);
+    }
+    
+
+    /**
+     * @Route("/category/edit/{slug}" , name="category_edit")
+     */
+    public function edit(Category $category, Request $request)
+    {
+        $form = $this->createForm( CategoryType::class ,$category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+           
+            $category->setSlug($this->slugger->slugify($category->getName()));
+
+            
+
+            $this->manager->flush();
+
+            $this->addFlash('success', 'Les modifications de la catégorie a bien été enregistée');
+
+            return $this->redirectToRoute('category');
+
+        }
+
+        return $this->render('category/form.html.twig', [
+            'form' => $form->createView(),
+            'category' => $category
+        ] );
+    }
+
+    /**
+     * @Route("/question/delete/{slug}" , name="category_delete")
+     */
+    public function delete (Category $category) 
+    {
+
+        $this->manager->remove($category);
+
+        $this->manager->flush();
+
+        $this->addFlash('success', 'La catégorie a bien été supprimée');
+
+        return $this->redirectToRoute('category');
     }
 }
